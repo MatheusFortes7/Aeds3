642343fe04b96668d0ca8e8f8f48172a795e6988
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2022-03-08 16:28:37 -0300

    uptade arquivo

--- Arquivo.java
+++ Arquivo.java
@@ -5,34 +5,64 @@ public class Arquivo<T extends Registro> {
 
   private RandomAccessFile arq;
   private Constructor<T> construtor;
+  private int TAMANHO_CABECALHO = 4;
 
   public Arquivo(Constructor<T> c, String nomeArquivo) throws Exception {
     arq = new RandomAccessFile(nomeArquivo, "rw");
     construtor = c;
+
+    // cabeçalho
+    if (arq.length() < TAMANHO_CABECALHO) {
+      arq.seek(0);
+      arq.writeInt(0); // ultimo ID
+    }
   }
 
-  public void create(T entidade) throws Exception {
+  public int create(T entidade) throws Exception {
+
+    // Determinar o ID da nova entidade e atualizar o cabeçalho
+    arq.seek(0);
+    int ultimoID = arq.readInt();
+    int novoID = ultimoID + 1;
+    arq.seek(0);
+    arq.writeInt(novoID);
+    entidade.setID(novoID);
+
+    // Encontra o ponto de escrita do novo registro
+    arq.seek(arq.length());
+
+    // Escreve o novo registro
     byte[] ba;
     ba = entidade.toByteArray();
     arq.seek(arq.length());
+    arq.writeByte(' '); // '*' será usado para registros excluídos
     arq.writeShort((short) ba.length);
     arq.write(ba);
+    return novoID;
   }
 
   public T read(int id) throws Exception {
     short tamanho;
     byte[] ba;
     T entidade = this.construtor.newInstance();
-    arq.seek(0);
+    byte lapide;
+    arq.seek(TAMANHO_CABECALHO); // pula os bytes do cabeçalho
     while (arq.getFilePointer() != arq.length()) {
+      lapide = arq.readByte();
       tamanho = arq.readShort();
       ba = new byte[tamanho];
       arq.read(ba);
-      entidade.fromByteArray(ba);
-      if (entidade.getID() == id)
-        return entidade;
+      if (lapide == ' ') {
+        entidade.fromByteArray(ba);
+        if (entidade.getID() == id)
+          return entidade;
+      }
     }
     return null;
+  }
+
+  public void close() throws Exception {
+    arq.close();
   }
 
 }
