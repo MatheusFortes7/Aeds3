fcb66444d4154e606af08143aa58a2087fe8c723
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2022-03-10 15:08:45 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,62 @@
+import java.io.File;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+class Main {
 
+  private static Scanner console = new Scanner(System.in);
+
+  public static void main(String args[]) {
+
+    File d;
+    d = new File("dados");
+    if (!d.exists())
+      d.mkdir();
+    d = new File("dados/clientes");
+    if (!d.exists())
+      d.mkdir();
+    d = new File("dados/livros");
+    if (!d.exists())
+      d.mkdir();
+
+    // Livro l1 = new Livro("Odisseia", "Homero", 15.99F);
+    // Livro l2 = new Livro("Ensino Híbrido", "Lilian Bacich", 39.90F);
+    // Cliente c1 = new Cliente("João Alves", "alves@gmail.com");
+    // Cliente c2 = new Cliente("Ana Paula Cardoso", "aninha@hotmail.com");
+    // int idLivro, idCliente;
+
+    try {
+      // Menu principal
+      int opcao;
+      do {
+        System.out.println("\n\nMenu principal");
+        System.out.println("--------------");
+        System.out.println("1) Livros");
+        System.out.println("2) Clientes");
+        System.out.println("0) Sair");
+        System.out.print("\nOpção: ");
+
+        try {
+          opcao = Integer.valueOf(console.nextLine());
+        } catch (NumberFormatException e) {
+          opcao = -1;
+        }
+
+        switch (opcao) {
+          case 1:
+            (new InteracaoLivros()).MenuLivros();
+            break;
+          case 2:
+            // InteracaoClientes.MenuClientes();
+            break;
+          case 0:
+            break;
+          default:
+            System.out.println("Opção inválida!");
+        }
+      } while (opcao != 0);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
+
 }
