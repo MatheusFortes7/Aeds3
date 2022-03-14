49ad9ff96ff166d0b1d51b1ebc6ca52c1b4d6052
Matheus Fortes <79404379+MatheusFortes7@users.noreply.github.com>
2022-03-14 14:48:00 -0300

    organização de arquivos

--- Main.java
+++ Main.java
@@ -1,62 +1,41 @@
-import java.io.File;
+
 import java.util.Scanner;
 
-class Main {
+public class Main {
 
   private static Scanner console = new Scanner(System.in);
 
-  public static void main(String args[]) {
+  public static void main(String[] args) {
 
-    File d;
-    d = new File("dados");
-    if (!d.exists())
-      d.mkdir();
-    d = new File("dados/clientes");
-    if (!d.exists())
-      d.mkdir();
-    d = new File("dados/livros");
-    if (!d.exists())
-      d.mkdir();
+    int opcao;
+    do {
+      System.out.println("\nMENU PRINCIPAL");
+      System.out.println("\n1) Livros");
+      System.out.println("2) Clientes");
+      System.out.println("\n0) Sair");
 
-    // Livro l1 = new Livro("Odisseia", "Homero", 15.99F);
-    // Livro l2 = new Livro("Ensino Híbrido", "Lilian Bacich", 39.90F);
-    // Cliente c1 = new Cliente("João Alves", "alves@gmail.com");
-    // Cliente c2 = new Cliente("Ana Paula Cardoso", "aninha@hotmail.com");
-    // int idLivro, idCliente;
+      System.out.print("\nOpção: ");
+      try {
+        opcao = Integer.valueOf(console.nextLine());
+      } catch (NumberFormatException e) {
+        opcao = -1;
+      }
 
-    try {
-      // Menu principal
-      int opcao;
-      do {
-        System.out.println("\n\nMenu principal");
-        System.out.println("--------------");
-        System.out.println("1) Livros");
-        System.out.println("2) Clientes");
-        System.out.println("0) Sair");
-        System.out.print("\nOpção: ");
+      switch (opcao) {
+        case 1:
+          (new entidades.livros.InteracoesLivros()).menuLivros();
+          break;
+        case 2:
+          // menu clientes;
+          break;
+        case 0:
+          break;
+        default:
+          System.out.println("Opção inválida");
+      }
 
-        try {
-          opcao = Integer.valueOf(console.nextLine());
-        } catch (NumberFormatException e) {
-          opcao = -1;
-        }
+    } while (opcao != 0);
 
-        switch (opcao) {
-          case 1:
-            (new InteracaoLivros()).MenuLivros();
-            break;
-          case 2:
-            // InteracaoClientes.MenuClientes();
-            break;
-          case 0:
-            break;
-          default:
-            System.out.println("Opção inválida!");
-        }
-      } while (opcao != 0);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
 }
---
+++ aed3/Arquivo.java
@@ -0,0 +1,79 @@
+package aed3;
+
+import java.io.RandomAccessFile;
+import java.lang.reflect.Constructor;
+
+public class Arquivo<T extends Registro> {
+
+  private RandomAccessFile arquivo;
+  private HashExtensivel<ParIDEndereco> indiceDireto;
+  private Constructor<T> construtor;
+  private int TAMANHO_CABECALHO = 4;
+
+  public Arquivo(String nomePasta, Constructor<T> c) throws Exception {
+    arquivo = new RandomAccessFile(nomePasta + "/dados.db", "rw");
+    indiceDireto = new HashExtensivel<>(
+        ParIDEndereco.class.getConstructor(),
+        4,
+        nomePasta + "/indiceID.1.db",
+        nomePasta + "/indiceID.2.db");
+    construtor = c;
+    if (arquivo.length() < TAMANHO_CABECALHO) {
+      arquivo.seek(0);
+      arquivo.writeInt(0);
+    }
+  }
+
+  public int create(T entidade) throws Exception {
+    arquivo.seek(0);
+    int ultimoID = arquivo.readInt();
+    int novoID = ultimoID + 1;
+    entidade.setID(novoID);
+    arquivo.seek(0);
+    arquivo.writeInt(novoID);
+
+    // Movimenta o ponteiro do arquivo para o ponto de inserção do novo registro
+    arquivo.seek(arquivo.length());
+    long endereco = arquivo.getFilePointer();
+
+    // Cria o registro no arquivo
+    byte[] ba = entidade.toByteArray();
+    arquivo.writeByte('#'); // # -> registro válido; * -> registro excluído
+    arquivo.writeShort(ba.length);
+    arquivo.write(ba);
+    indiceDireto.create(new ParIDEndereco(novoID, endereco));
+    return novoID;
+  };
+
+  public T read(int id) throws Exception {
+
+    ParIDEndereco p = indiceDireto.read(id);
+    if (p == null)
+      return null;
+
+    arquivo.seek(p.getEndereco());
+
+    byte lapide = arquivo.readByte();
+    int tamanho = arquivo.readShort();
+    byte[] ba = new byte[tamanho];
+    if (lapide == '#') {
+      arquivo.read(ba);
+      T entidade = construtor.newInstance();
+      entidade.fromByteArray(ba);
+      if (entidade.getID() == id)
+        return entidade;
+    }
+
+    System.out.println("Arquivo corrompido.");
+    return null;
+  };
+
+  public boolean update(T novaEntidade) {
+    return false;
+  };
+
+  public boolean delete(int id) {
+    return false;
+  };
+
+}
